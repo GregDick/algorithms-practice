462f47fcaa517d8e55f011f5098e28a6aabce781
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-09-08 15:50:00 -0500

    added bubble sort and abstracted insertion to a function

--- main.js
+++ main.js
@@ -1,39 +1,63 @@
-var start = _.shuffle(_.range(30));
+var start = _.shuffle(_.range(20));
 $(".start").append(start.toString());
 
 var iterations = 0;
-
-//insertion sort
 var answer = [];
 
-start.reduce(function(prev, curr, i){
-  prev.push(curr);
-  for(var x = prev.length-1; x > 0 ; x--){
+//insertion sort
+function insertion (arr, answer) {
+  arr.reduce(function(prev, curr, i){
+    prev.push(curr);
+    for(var x = prev.length-1; x > 0 ; x--){
 
-    $("answer").append(prev.toString());
-    iterations ++;
-    if(prev[x-1] > prev[x]){
-      var placeholder = prev[x];
-      prev[x]         = prev[x-1];
-      prev[x-1]       = placeholder;
-    } else if(prev[x-1] < prev[x]){
-      break;
+      $("answer").append(prev.toString());
+      iterations ++;
+      if(prev[x-1] > prev[x]){
+        var placeholder = prev[x];
+        prev[x]         = prev[x-1];
+        prev[x-1]       = placeholder;
+      } else if(prev[x-1] < prev[x]){
+        break;
+      }
+
     }
+    return prev;
+  }, answer);
 
+  $(".answer").append('insertion: '+answer.toString());
+}
+
+//bubble sort
+function bubbles (arr){
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i] > arr[i+1]){
+      var placeholder = arr[i+1];
+      arr[i+1] = arr[i];
+      arr[i] = placeholder;
+      console.log(arr);
+      count ++;
+    }
+    iterations++;
+  };
+  if(count===0){
+    $(".answer").append('bubbles: ' + start.toString());
+    return arr;
+  }else{
+    return bubbles(arr);
   }
-  return prev;
-}, answer);
+}
 
-$(".answer").append(answer.toString());
+//selection sort
+function selectionSort (arr){
+
+}
+
+
+
+// insertion(start, answer);
+bubbles(start);
+
+
 $(".iterations").append(iterations);
 
-
-// function checkPair (prev, x) {
-//   $("answer").append(prev.toString());
-//   iterations ++;
-//   if(prev[x-1] > prev[x]){
-//     prev.splice(x-1, 2, prev[x], prev[x-1]);
-//   } else if(prev[x-1] < prev[x]){
-//     break;
-//   }
-// }
